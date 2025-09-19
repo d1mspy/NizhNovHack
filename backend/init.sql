fc8551328bb657cdb3a24c722897c4e5b4a4aa32
d1mspy <subbotin2126@gmail.com>
2025-09-20 01:07:07 +0300

    feat: add user table

--- backend/init.sql
+++ backend/init.sql
@@ -1,9 +1,29 @@
-/* тут будет инициализация бд когда бд будет спроектирована */
+CREATE TYPE sex_enum AS ENUM ('male', 'female');
 
-/* тестовая инициализация */
-CREATE TABLE test(
-    id uuid primary key,
-    created_at timestamp not null,
-    updated_at timestamp not null,
-    string text
-)
+CREATE TABLE "user"(
+    id UUID PRIMARY KEY,
+
+    first_name TEXT NOT NULL CHECK (btrim(first_name) <> '' AND char_length(first_name) <= 50),
+    last_name  TEXT NOT NULL CHECK (btrim(last_name)  <> '' AND char_length(last_name)  <= 50),
+
+    sex sex_enum NOT NULL,
+    birth_date DATE NOT NULL CHECK (birth_date <= CURRENT_DATE 
+                                    AND birth_date > CURRENT_DATE - INTERVAL '120 years'),
+
+    current_position TEXT NOT NULL CHECK (current_position ~ '[^[:space:]]'),
+
+    education TEXT,
+
+    experience_years SMALLINT NOT NULL DEFAULT 0 CHECK (experience_years >= 0 AND experience_years <= 80),
+    experience_months SMALLINT NOT NULL DEFAULT 0 CHECK (experience_months BETWEEN 0 AND 11),
+
+    experience_total_months INTEGER GENERATED ALWAYS AS (experience_years * 12 + experience_months) STORED,
+
+    experience_description TEXT,
+
+    hard_skills TEXT[] NOT NULL DEFAULT ARRAY[]::TEXT[],
+
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
+);
+
--- backend/presentations/app.py
+++ backend/presentations/app.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from repositories.db.repository import Repository
 
-app = FastAPI(title="хак")
+app = FastAPI(title="Т1 хак")
 
 app.add_middleware(
     CORSMiddleware,
@@ -13,31 +12,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# тестовая pydantic модель
-class Testdb(BaseModel):
-    string: str
-
-# экземпляр тестового класса для взаимодействия с бд
-repository = Repository()
-
 @app.get("/")
 async def test_endpoint() -> str:
     """
     тестовый эндпоинт
     """
     return "ok"
-
-@app.post("/test")
-async def test_post(testdb: Testdb) -> None:
-    """
-    тест базы данных
-    """
-    await repository.test_post(testdb.string)
-
-@app.get("/test")
-async def test_get() -> list | None:
-    """
-    тест базы данных
-    """
-    data = await repository.test_get()
-    return data
