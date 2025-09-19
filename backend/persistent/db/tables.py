943af1132ccaacdade404fea978db3e3d38f607b
d1mspy <subbotin2126@gmail.com>
2025-09-20 02:30:00 +0300

    feat: add vacancy table

--- backend/persistent/db/tables.py
+++ backend/persistent/db/tables.py
@@ -55,3 +55,22 @@ class User(Base, WithId, With_created_at, With_updated_at):
                         name="ck_user_experience_months"),
         {"extend_existing": True},
     )
+    
+class Vacancy(Base, WithId, With_created_at, With_updated_at):
+    __tablename__ = "vacancy"
+    
+    description = Column(Text, nullable=False)
+    
+    min_exp_months = Column(SmallInteger, nullable=True)
+    max_exp_months = Column(SmallInteger, nullable=True)
+    
+    must_have = Column(ARRAY(Text), nullable=False, server_default=text("'{}'::text[]"))
+    nice_to_have = Column(ARRAY(Text), nullable=False, server_default=text("'{}'::text[]"))
+
+    __table_args__ = (
+        CheckConstraint(
+            "(min_exp_months IS NULL) OR (max_exp_months IS NULL) "
+            "OR (min_exp_months <= max_exp_months)",
+            name="vacancy_min_max_check",
+        )
+    )
