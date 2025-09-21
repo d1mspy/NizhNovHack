d52bf45b5ee24d91fdd80f180d1b5bed32e8645e
d1mspy <subbotin2126@gmail.com>
2025-09-21 15:32:20 +0300

    hotfix

--- backend/ai_services/utils/career_agent.py
+++ backend/ai_services/utils/career_agent.py
@@ -7,15 +7,17 @@ from openai import AsyncOpenAI
 
 class CareerAgent:
     def __init__(self, api_key: str):
-        self.llm_client = AsyncOpenAI(api_key=api_key, base_url="https://llm.t1v.scibox.tech/v1")
+        self.llm_client = AsyncOpenAI(
+            api_key=api_key, base_url="https://llm.t1v.scibox.tech/v1")
         self.model_name = "Qwen2.5-72B-Instruct-AWQ"
-    
+
     async def get_response(self, message_history):
 
         response = await self.llm_client.chat.completions.create(
-                    model=self.model_name,
-                    messages=message_history,
-                    temperature=0.1,
-                    max_tokens=4000
-                )
+            model=self.model_name,
+            messages=message_history,
+            temperature=0.1,
+            max_tokens=4000
+        )
         return response.choices[0].message.content
+    
